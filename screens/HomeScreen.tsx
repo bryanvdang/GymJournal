1ec6a5e4cb5b3493026886685b74e9af177afc2e
Bryan Dang <bryanvdang@gmail.com>
2022-06-23 21:51:08 -0700

    created component for each workout item and styled it

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,12 +1,31 @@
-import { SafeAreaView, Text, Button } from "react-native";
+import { SafeAreaView, FlatList, StyleSheet, Text } from "react-native";
 import { NativeStackHeaderProps } from "@react-navigation/native-stack";
 import data from "../data.json"
+import { Workout } from "../types/data";
+import WorkoutItem from "../components/WorkoutItem";
 
 export default function HomeScreen({navigation}: NativeStackHeaderProps) {
     return(
-        <SafeAreaView>
-            <Text>I am Home Screen</Text>
-            <Text>{ JSON.stringify(data)}</Text>
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.header}>New Workout</Text>
+            <FlatList data={data as Workout[]}
+            renderItem={WorkoutItem}
+            keyExtractor={item=> item.slug}
+            />
         </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        // padding: 20,
+        flex: 1,
+    },
+    header: {
+        fontSize: 20,
+        paddingTop: 10,
+        marginBottom: 10,
+        marginLeft: 10,
+        fontWeight: 'bold'
+    }
+})
